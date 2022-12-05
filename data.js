df4eb219d83a461793118ee3f0622b4359d53f79
omar chatin <omerchatin19@gmail.com>
2022-12-05 22:17:03 +0300

    s

--- data.js
+++ data.js
@@ -3,21 +3,8 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 const client = new ApolloClient({
-  uri: "https://api-ap-south-1.hygraph.com/v2/clauzcz0i2dk401uschsk4wk3/master",
+  uri: process.env.GRAPHCMS_TOKEN,
   cache: new InMemoryCache(),
 });
 
-const SanityClient = createClient({
-  projectId: "9zeiwwka",
-  dataset: "production",
-  apiVersion: "2022-03-25",
-  useCdn: false,
-});
-const builder = imageUrlBuilder(SanityClient);
-
-function ImageUrlFor(source) {
-  return builder.image(source);
-}
-
-export { SanityClient, ImageUrlFor };
 export default client;
