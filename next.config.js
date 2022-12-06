0293ee152d8b271e3ef51a73bfec230076c42444
omar chatin <omerchatin19@gmail.com>
2022-12-06 18:10:37 +0300

    ewr4e

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  // useTranslate: true,
-  // useTranslateConfig: {
-  //   defaultLanguage: "ar",
-  //   supportedLanguages: ["en", "ar"],
-  //   defaultLocale: "ar",
-  //   supportedLocales: ["en", "ar"],
-  // },
 };
 
-module.exports = nextConfig;
+const nextTranslate = require("next-translate");
+
+module.exports = nextTranslate({
+  webpack: (config, { isServer, webpack }) => {
+    return config;
+  },
+});
