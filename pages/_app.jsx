6137a052e23114a006054ecfed746721cc857773
omar chatin <omerchatin19@gmail.com>
2022-12-09 18:45:59 +0300

    s

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import { MainLayout } from "../components";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <MainLayout>
       <Component {...pageProps} />
+      <Analytics />
     </MainLayout>
   );
 }
